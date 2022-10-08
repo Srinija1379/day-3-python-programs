6e7ba3b3afd39e43d87f052aca58b7e6d94cdbea
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-08 11:51:52 +0530

    Add files via upload

---
+++ reverse number output.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\reverse number.py 
+enter the numbers123456
+Reversed Number: 654321
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\reverse number.py 
+enter the numbers5678
+Reversed Number: 8765
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\reverse number.py 
+enter the numbers99879
+Reversed Number: 97899
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\reverse number.py 
+enter the numbers9890
+Reversed Number: 989
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\reverse number.py 
+enter the numbers1234
+Reversed Number: 4321
+>>> 
